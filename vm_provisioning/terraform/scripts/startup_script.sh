5b1d68604e3aa3d36908f1c21703269608411880
Mateo Di Loreto <mateoadiloreto@gmail.com>
2024-08-11 19:39:41 -0300

    update deploy.yaml become and startupscript

--- vm_provisioning/terraform/scripts/startup_script.sh
+++ vm_provisioning/terraform/scripts/startup_script.sh
@@ -13,20 +13,20 @@ sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings 
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc 
 sudo chmod a+r /etc/apt/keyrings/docker.asc 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || { echo "Failed to add Docker repository"; exit 1; }
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keydockerrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || { echo "Failed to add Docker repository"; exit 1; }
 sudo apt-get update -y 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
 sudo apt-get update
-sudo apt-get install docker-compose-plugin
+sudo apt-get install docker-compose-plugin -y
 sudo docker --version
-docker network create app_net
+sudo docker network create app_net
 
 ## Install community.docker
 
-ansible-galaxy collection install community.docker
+sudo ansible-galaxy collection install community.docker
 
 ## Clone repo and execute playbook
 
 git clone https://github.com/mdiloreto/poc-ansible-haproxy-echoserver-nginx /tmp/haproxy-poc
 cd /tmp/haproxy-poc/playbooks/
-ansible-playbook deploy.yml
+sudo ansible-playbook deploy.yml
