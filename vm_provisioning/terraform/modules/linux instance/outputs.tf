affe76555bbebbb22adb5c814792fbf4d5c8e1cb
Mateo Di Loreto <mateoadiloreto@gmail.com>
2024-08-11 22:49:33 -0300

    update HAProxy sessions persistence

--- vm_provisioning/terraform/modules/linux instance/outputs.tf
+++ vm_provisioning/terraform/modules/linux instance/outputs.tf
@@ -1,12 +1,21 @@
-output "ipv4-addresses" {
-  value = [for instance in google_compute_instance.default : instance.network_interface[0].network_ip]
-}
+  output "ipv4-addresses" {
+    value = [for instance in google_compute_instance.default : instance.network_interface[0].network_ip]
+  }
 
-output "instance_public_ips" {
-  value = [ for instance in google_compute_instance.default : instance.network_interface[0].access_config[0].nat_ip ]
-  description = "The public IP addresses of the VM instances"
-}
+  output "instance_public_ips" {
+    value = [ for instance in google_compute_instance.default : instance.network_interface[0].access_config[0].nat_ip ]
+    description = "The public IP addresses of the VM instances"
+  }
 
-output "self_link" {
-  value = [ for instance in google_compute_instance.default : instance.self_link ]
-}
+  output "self_link" {
+    value = [ for instance in google_compute_instance.default : instance.self_link ]
+  }
+
+  output "zone" {
+    value = [ for instance in google_compute_instance.default : instance.zone ]
+  }
+
+  output "vm_name" {
+    value = [ for instance in google_compute_instance.default : instance.vm_name ]
+  }
+
